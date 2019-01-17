486a8bbd45af0d8ee0e8e447233a1f8fd2236a90
dalmariko <dalmarikocamp@gmail.com>
2019-01-17 22:14:42 +0200

    get query add with promis all

--- js/mastercontroller.js
+++ js/mastercontroller.js
@@ -6,10 +6,10 @@ const config = {
 
 const state = {};
 
-let countrys = ['ua', 'us', 'gb'];
+let countrys = ['ua', 'us', 'gb','ru'];
 let categorys = ['business', 'entertainment', 'general', 'health', 'science', 'technology'];
 let categorysInBase = [];
-
+let queryArreys=[];
 
 const http = new Fetch();
 const base = new DBFirebase();
@@ -53,8 +53,7 @@ const makeName = () => {
 };
 
 
-const grabeApi = () => {
-
+const prepereAllQuerys = ()=> {
     const makeQuery = (country, category) => {
         return `${config.api_url}/top-headlines?country=${country}&category=${category}&apiKey=${config.api_key}`;
     };
@@ -67,7 +66,18 @@ const grabeApi = () => {
 
             let query = makeQuery(country, category);
 
-            http.get(query)
+            queryArreys.push(query);
+        }
+    }
+return;
+};
+const grabeApi = () => {
+
+    prepereAllQuerys();
+
+    Promise.all(
+        queryArreys.map(oneQuery => {
+            return http.get(oneQuery)
                 .then(res => {
                     let temp = new Array();
                     res.articles.forEach((news) => {
@@ -78,146 +88,125 @@ const grabeApi = () => {
                 })
                 .then(prepareNews => {
                     prepareNews.forEach(news => {
-                        base.saveDBNews(collectionName, news);
+                        // base.saveDBNews(collectionName, news);
                     });
-                    console.log('dates addet to collection', collectionName, '\n');
+                    console.log('Новость добавленна в Коллекцию', '\n');
                 })
-                .catch(err => console.log(err));
-
-        }
-    }
-};
-
-const getTimeLabel = () => {
-    return base.getTimeLebel()
-        .then(labelS => {
-            let allLabeles = {
-                pullLabels: new Array()
-            };
-            labelS.forEach(doc => {
-                let oneDoc = {
-                    dateId: doc.id,
-                    isGrabe: doc.data().isGrabe,
-                    timeStemp: doc.data().timeStemp * 1,
-                };
-                allLabeles.pullLabels.push(oneDoc);
-            });
-            return allLabeles;
-        })
-        .then(oldTimeS => {
-            lastTimeUpdateBase = oldTimeS.pullLabels.sort((a, b) => {
-                return b.timeStemp - a.timeStemp;
-            })[0];
-        })
-        .catch(err => console.log(err));
-};
-
-const getIPinfo = () => {
-    ip.get('http://www.geoplugin.net/json.gp')
-        .then(response => {
-            let fresh;
-            for (let item in response) {
-                newIPData = {
-                    request: response['geoplugin_request'],
-                    city: response['geoplugin_city'],
-                    region: response['geoplugin_region'],
-                    regionCode: response['geoplugin_regionCode'],
-                    regionName: response['geoplugin_regionName'],
-                    countryCode: response['geoplugin_countryCode'],
-                    countryName: response['geoplugin_countryName'],
-                    continentCode: response['geoplugin_continentCode'],
-                    latitude: response['geoplugin_latitude'],
-                    longitude: response['geoplugin_longitude'],
-                    timezone: response['geoplugin_timezone'],
-                    currencyCode: response['geoplugin_currencyCode'],
-                    currencyConverter: response['geoplugin_currencyConverter'],
-                }
-            }
-            return newIPData;
-        })
-        .catch(err => console.log(err));
-};
-
-
-const getNewsFromBase = () => {
-    makeName();
-
-    categorysInBase.forEach((nameOfCategory) => {
-        state[nameOfCategory] = [];
-    });
-
-    return Promise.all(
-        categorysInBase.map(nameOfCategory => {
-            return base.getDBNews(nameOfCategory)
-                .then(item => {
-                    item.forEach(doc => {
-                        state[nameOfCategory][state[nameOfCategory].length] = doc.data();
-                    });
-                });
-        }))
-        .then((rez) => {
-            rez.forEach(item => {
-                console.log(item, Date.now());
-            });
-            console.log('достали из базы все категории по 200 новостей');
-        })
+                .catch(err => console.log(err.message));
+        }).reduce((sequence, chapterPromise) => {
+                return sequence.then(() => {
+                    return chapterPromise;
+                })
+            })
+    )
         .catch(err => console.log(err));
 
 };
 
+grabeApi();
 
+// const getTimeLabel = () => {
+//     return base.getTimeLebel()
+//         .then(labelS => {
+//             let allLabeles = {
+//                 pullLabels: new Array()
+//             };
+//             labelS.forEach(doc => {
+//                 let oneDoc = {
+//                     dateId: doc.id,
+//                     isGrabe: doc.data().isGrabe,
+//                     timeStemp: doc.data().timeStemp * 1,
+//                 };
+//                 allLabeles.pullLabels.push(oneDoc);
+//             });
+//             return allLabeles;
+//         })
+//         .then(oldTimeS => {
+//             lastTimeUpdateBase = oldTimeS.pullLabels.sort((a, b) => {
+//                 return b.timeStemp - a.timeStemp;
+//             })[0];
+//         })
+//         .catch(err => console.log(err));
+// };
+//
+// const getIPinfo = () => {
+//     ip.get('http://www.geoplugin.net/json.gp')
+//         .then(response => {
+//             let fresh;
+//             for (let item in response) {
+//                 newIPData = {
+//                     request: response['geoplugin_request'],
+//                     city: response['geoplugin_city'],
+//                     region: response['geoplugin_region'],
+//                     regionCode: response['geoplugin_regionCode'],
+//                     regionName: response['geoplugin_regionName'],
+//                     countryCode: response['geoplugin_countryCode'],
+//                     countryName: response['geoplugin_countryName'],
+//                     continentCode: response['geoplugin_continentCode'],
+//                     latitude: response['geoplugin_latitude'],
+//                     longitude: response['geoplugin_longitude'],
+//                     timezone: response['geoplugin_timezone'],
+//                     currencyCode: response['geoplugin_currencyCode'],
+//                     currencyConverter: response['geoplugin_currencyConverter'],
+//                 }
+//             }
+//             return newIPData;
+//         })
+//         .catch(err => console.log(err));
+// };
+//
 //
 // const getNewsFromBase = () => {
 //     makeName();
-//     categorysInBase.forEach((oneCategory)=>{
-//         state[oneCategory]=[];
-//         return base.getDBNews(oneCategory)
-//             .then( item => {
-//                 item.forEach( doc=>{
-//                     state[oneCategory][state[oneCategory].length] = doc.data();
-//                 });
-//             })
-//             .then(()=>{
-//                 console.log('достали из базы все категории по 200 новостей');
-//             })
-//             .catch(err => console.log(err));
+//
+//     categorysInBase.forEach((nameOfCategory) => {
+//         state[nameOfCategory] = [];
 //     });
 //
+//     return Promise.all(
+//         categorysInBase.map(nameOfCategory => {
+//             return base.getDBNews(nameOfCategory)
+//                 .then(item => {
+//                     item.forEach(doc => {
+//                         state[nameOfCategory][state[nameOfCategory].length] = doc.data();
+//                     });
+//                 })
+//                 .catch(err => console.log(err));
+//         }))
+//         .then( ()=> {
+//             console.log('достали из базы все категории по 200 новостей');
+//         })
+//         .catch(err => console.log(err));
+//
 // };
-
-getNewsFromBase();
-console.log(state);
-
-
-const goNextLoop = () => {
-    Promise.all([getNewsFromBase(), getTimeLabel()])
-        .then(timerGo => {
-            compareLabelTime();
-        })
-        .catch(err => console.log(err));
-};
-
-
-const compareLabelTime = () => {
-    let handle;
-    handle = setTimeout(function get() {
-        let nowTime = Date.now();
-
-        console.log('Время сейчас =', nowTime, '|', 'Время последнего обновления = ', lastTimeUpdateBase.timeStemp, '|', nowTime - lastTimeUpdateBase.timeStemp);
-
-        if (nowTime - lastTimeUpdateBase.timeStemp > 1200000 && lastTimeUpdateBase.isGrabe === false) {
-            grabeApi();
-            base.addTimeLebel({isGrabe: false, timeStemp: Date.now() + ''});
-            base.setTimeLebel(lastTimeUpdateBase.dateId, {isGrabe: true, timeStemp: lastTimeUpdateBase.timeStemp + ''});
-            console.log('проверка метки успешна, новая метка добавлена, старая метка изменена');
-            clearTimeout(handle);
-            return goNextLoop();
-        }
-        console.log(lastTimeUpdateBase);
-        console.log('проверяем локально');
-        handle = setTimeout(get, 10000);
-    }, 10);
-};
-
+//
+//
+// const goNextLoop = () => {
+//     Promise.all([getNewsFromBase(), getTimeLabel()])
+//         .then(timerGo => {
+//             compareLabelTime();
+//         })
+//         .catch(err => console.log(err));
+// };
+//
+// const compareLabelTime = () => {
+//     let handle;
+//     handle = setTimeout(function get() {
+//         let nowTime = Date.now();
+//         if (nowTime - lastTimeUpdateBase.timeStemp > 2400000 && lastTimeUpdateBase.isGrabe === false) {
+//             grabeApi();
+//             base.addTimeLebel({isGrabe: false, timeStemp: Date.now() + ''});
+//             base.setTimeLebel(lastTimeUpdateBase.dateId, {isGrabe: true, timeStemp: lastTimeUpdateBase.timeStemp + ''});
+//             console.log('проверка метки успешна, новая метка добавлена, старая метка изменена');
+//             clearTimeout(handle);
+//             return goNextLoop();
+//         }
+//         console.log('проверяем локально');
+//         console.log('Время сейчас =', nowTime, '|', 'Время последнего обновления = ', lastTimeUpdateBase.timeStemp, '|', nowTime - lastTimeUpdateBase.timeStemp);
+//
+//         handle = setTimeout(get, 1000000);
+//     }, 1000000);
+// };
+//
 // goNextLoop();
 

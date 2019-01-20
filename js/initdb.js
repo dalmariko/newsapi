301388b7011c7611e436b567bb78c40769c92324
dalmariko <dalmarikocamp@gmail.com>
2019-01-20 15:01:45 +0200

    add one db

--- js/initdb.js
+++ js/initdb.js
@@ -1,33 +1,10 @@
 const firebaseConfig = {
-    // apiKey: "AIzaSyD6N_N0FHIcTyiN-6bCubaZ8d9H-OUHG6c",
-    // authDomain: "redpress-b2935.firebaseapp.com",
-    // databaseURL: "https://redpress-b2935.firebaseio.com",
-    // projectId: "redpress-b2935",
-    // storageBucket: "redpress-b2935.appspot.com",
-    // messagingSenderId: "502141343996"
-
-    // apiKey: "AIzaSyCzo-Sds4Srp4StIIp2nymYW9QBCM5YM5o",
-    // authDomain: "spernews.firebaseapp.com",
-    // databaseURL: "https://spernews.firebaseio.com",
-    // projectId: "spernews",
-    // storageBucket: "spernews.appspot.com",
-    // messagingSenderId: "1082484240233"
-
-
-    // apiKey: "AIzaSyAc-BppSV1ckVAn5or8yjtTcCDxspcPKh0",
-    // authDomain: "testpromises.firebaseapp.com",
-    // databaseURL: "https://testpromises.firebaseio.com",
-    // projectId: "testpromises",
-    // storageBucket: "testpromises.appspot.com",
-    // messagingSenderId: "915131080184"
-
-    apiKey: "AIzaSyCC_PI7KwuYzNT4pmbdYw1nL1sayt-9T5A",
-    authDomain: "mymegapress.firebaseapp.com",
-    databaseURL: "https://mymegapress.firebaseio.com",
-    projectId: "mymegapress",
-    storageBucket: "mymegapress.appspot.com",
-    messagingSenderId: "55838716029"
-
+    apiKey: "AIzaSyDGE1CgbFtYYwKwsOGJQnYckRSIGIiRdjo",
+    authDomain: "superredpress.firebaseapp.com",
+    databaseURL: "https://superredpress.firebaseio.com",
+    projectId: "superredpress",
+    storageBucket: "superredpress.appspot.com",
+    messagingSenderId: "422438773510"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();

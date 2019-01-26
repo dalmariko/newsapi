8486e5ff0dceecf38376284d9622cd28e691f2ed
dalmariko <dalmarikocamp@gmail.com>
2019-01-26 13:31:10 +0200

    fix bug:no worck getNewsFromBase() when page start, fix -> return add

--- js/initdb.js
+++ js/initdb.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDGE1CgbFtYYwKwsOGJQnYckRSIGIiRdjo",
-    authDomain: "superredpress.firebaseapp.com",
-    databaseURL: "https://superredpress.firebaseio.com",
-    projectId: "superredpress",
-    storageBucket: "superredpress.appspot.com",
-    messagingSenderId: "422438773510"
+    apiKey: "AIzaSyAbXrun_64uBTL3kuurmZwdy8d04OvY3p8",
+    authDomain: "redpress-c6144.firebaseapp.com",
+    databaseURL: "https://redpress-c6144.firebaseio.com",
+    projectId: "redpress-c6144",
+    storageBucket: "",
+    messagingSenderId: "608049167412"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
--- js/mastercontroller.js
+++ js/mastercontroller.js
@@ -3,23 +3,29 @@ const config = {
     api_key: 'ea10580709394a6487ddd7d48952b1f1',
 };
 
-const state = {};
-const TIMEUPDATE=7200000;
+let state = {};
 
+// const TIMEUPDATE=7200000;
+const TIMEUPDATE = 1800000;
 
-
-const countrys = ['us','ua','fr','de'];
 const categorys = ['business', 'entertainment', 'general', 'health', 'science', 'technology'];
+// const countrys = ['us','ua','fr','de','ca','cu','no','se'];
+const countrys = ['ua'];
+// const categorys = ['general'];
 
 let categorysInBase = [];
 let queryArreys = [];
+
+
 
 const http = new Fetch();
 const base = new DBFirebase();
 const ip = new Fetch();
 
+// 'https://newsapi.org/v2/top-headlines?country=ru&category=general&apiKey=ea10580709394a6487ddd7d48952b1f1'
+
 let newIPData;
-let lastTimeUpdateBase;
+let lastTimeUpdateBase = '';
 
 const makeName = () => {
     for (let i = 0; i < countrys.length; i++) {
@@ -32,47 +38,75 @@ const makeName = () => {
     }
 };
 
-const prepereAllQuerys = () => {
-
-    for (let i = 0; i < countrys.length; i++) {
-        for (let k = 0; k < categorys.length; k++) {
-            let country = countrys[i];
-            let category = categorys[k];
-            let query = makeQuery(country, category);
-            queryArreys.push(query);
-        }
-    }
-};
-
 const makeQuery = (country, category) => {
     return `${config.api_url}/top-headlines?country=${country}&category=${category}&apiKey=${config.api_key}`;
 };
 
+const getNewsFromBase = () => {
+
+    makeName();
+
+        categorysInBase.forEach(nameOfCategory => {
+            state[nameOfCategory] = [];
+        });
+
+        return Promise.all(
+            categorysInBase.map(nameOfCategory => {
+                return base.getDBNews(nameOfCategory)
+                    .then(item => {
+                        item.forEach(doc => {
+                            state[nameOfCategory][state[nameOfCategory].length] = doc.data();
+                        });
+                    })
+                    .catch(err => console.log(err.message));
+            })
+        )
+            .then(() => {
+                console.log('достали из базы все категории по 200 новостей максимум за раз');
+            })
+            .then(() => {
+                ui.clearContainer();
+                return state['UAgeneral'].map(news => {
+                    ui.addNews(news);
+                })
+                    .reduce((secuence, chapterPromis) => {
+                        return secuence
+                            .then(() => {
+                                return chapterPromis;
+                            }).then(chapter => {
+                                return chapter;
+                            });
+                    }, Promise.resolve());
+            })
+            .catch(err => console.log(err.message));
+
+};
 
 const grabeApi = () => {
 
-
-    let allGETpromises=[];
+    let allGETpromises = [];
 
     for (county in countrys) {
         for (category in categorys) {
-           let query = makeQuery(countrys[county], categorys[category]);
+            let query = makeQuery(countrys[county], categorys[category]);
             let collectionName = countrys[county].toUpperCase() + categorys[category];
-           allGETpromises.push(
-                http.get( query )
+            allGETpromises.push(
+                http.get(query)
                     .then(res => {
-                    res.articles.forEach(news => {
-                        news.id = SHA256(Date.now() + news.title);
-                        base.saveDBNews(collectionName, news);
+                        res.articles.forEach(news => {
+                            news.id = SHA256(Date.now() + news.title);
+                            base.saveDBNews(collectionName, news);
+                        })
                     })
-                })
                     .catch(err => console.log(err.message))
             );
         }
     }
 
     Promise.all(allGETpromises)
-        .then(()=>{console.log('свежие новости полученны и сохранены')})
+        .then(() => {
+            console.log('свежие новости полученны и сохранены')
+        })
         .catch(err => console.log(err.message));
 
 };
@@ -127,65 +161,19 @@ const getTimeLabel = () => {
 //         .catch(err => console.log(err));
 // };
 
-const getNewsFromBase = () => {
-    makeName();
+const compareTimeLabel = () => {
 
-    categorysInBase.forEach( nameOfCategory => {
-        state[nameOfCategory] = [];
-    });
+    let nowTime = Date.now();
 
-    return Promise.all(
-        categorysInBase.map( nameOfCategory => {
-            return base.getDBNews(nameOfCategory)
-                .then(item => {
-                    item.forEach(doc => {
-                        state[nameOfCategory][state[nameOfCategory].length] = doc.data();
-                    });
-                })
-                .catch(err => console.log(err.message));
-        })
-    )
-        .then(() => {
-            console.log('достали из базы все категории по 200 новостей максимум за раз');
-        })
-        .then(() => {
-            return state['UAgeneral'].map(news => {
-                ui.addNews(news);
-            })
-                .reduce((secuence, chapterPromis) => {
-                    return secuence
-                        .then(() => {
-                            return chapterPromis;
-                        }).then(chapter => {
-                            return chapter;
-                        });
-                }, Promise.resolve());
-        })
-        .catch(err => console.log(err.message));
+    if (nowTime - lastTimeUpdateBase.timeStemp > TIMEUPDATE && lastTimeUpdateBase.isGrabe === false) {
+        grabeApi();
+        base.addTimeLebel({isGrabe: false, timeStemp: Date.now() + ''});
+        base.setTimeLebel(lastTimeUpdateBase.dateId, {isGrabe: true, timeStemp: lastTimeUpdateBase.timeStemp + ''});
+        console.log('новая метка добавлена, старая метка изменена');
+        getNewsFromBase();
+    }
 
-};
-
-const goNextLoop = () => {
-    Promise.all([getTimeLabel(),getNewsFromBase()])
-        .then(timerGo => {
-            compareLabelTime();
-        })
-        .catch(err => console.log(err.message));
-};
-
-const compareLabelTime = () => {
-    let handle;
-    handle = setTimeout(function get() {
-        let nowTime = Date.now();
-        if (nowTime - lastTimeUpdateBase.timeStemp > TIMEUPDATE && lastTimeUpdateBase.isGrabe === false) {
-            grabeApi();
-            base.addTimeLebel({isGrabe: false, timeStemp: Date.now() + ''});
-            base.setTimeLebel(lastTimeUpdateBase.dateId, {isGrabe: true, timeStemp: lastTimeUpdateBase.timeStemp + ''});
-            console.log('проверка метки успешна, новая метка добавлена, старая метка изменена');
-            clearTimeout(handle);
-            return goNextLoop();
-        }
-        let date = new Date(lastTimeUpdateBase.timeStemp+TIMEUPDATE);
+        let date = new Date(lastTimeUpdateBase.timeStemp + TIMEUPDATE);
         let options = {
             era: 'short',
             year: 'numeric',
@@ -198,10 +186,25 @@ const compareLabelTime = () => {
             second: 'numeric'
         };
         let lng = navigator.browserLanguage || navigator.language || navigator.userLanguage;
+        console.log(`Обновление Базы новостей поризойдет: - "${date.toLocaleString(`${lng}`, options)}", пока проверяем локально.`);
 
-        console.log(`Обновление Базы новостей поризойдет: - "${date.toLocaleString(`${lng}`,options)}", пока проверяем локально.`);
-        handle = setTimeout(get, TIMEUPDATE);
-    }, 10);
 };
 
-goNextLoop();
+
+let handle;
+handle = setTimeout(function get() {
+    Promise.resolve(getTimeLabel())
+        .then(() => {
+          // Object.keys(state).length!=0?console.log('state не пустой !!!!!!'):getNewsFromBase();
+          return getNewsFromBase();
+    })
+        .then(() => {
+          return compareTimeLabel();
+    })
+        .catch(err => {
+            console.log(err.message)
+        });
+    handle = setTimeout(get, TIMEUPDATE);
+}, 10);
+
+
